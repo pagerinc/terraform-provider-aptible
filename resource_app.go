51f5b95ae65bcb9c47d4db227abf7acb43b047c3
Gaston Festari <cilindrox@gmail.com>
2019-02-22 11:25:24 -0300

    feat: initial commit

---
+++ resource_app.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceServerCreate,
+		Read:   resourceServerRead,
+		Update: resourceServerUpdate,
+		Delete: resourceServerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
