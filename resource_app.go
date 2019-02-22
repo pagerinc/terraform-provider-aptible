a6251b09f5b14c859fc12e27ad0efdbb59d5d9c0
Gaston Festari <cilindrox@gmail.com>
2019-02-22 17:17:00 -0300

    feat: resource app

--- resource_app.go
+++ resource_app.go
@@ -4,18 +4,38 @@ import (
 	"github.com/hashicorp/terraform/helper/schema"
 )
 
-func resourceServer() *schema.Resource {
+func resourceApp() *schema.Resource {
 	return &schema.Resource{
-		Create: resourceServerCreate,
-		Read:   resourceServerRead,
-		Update: resourceServerUpdate,
-		Delete: resourceServerDelete,
+		Create: resourceAppCreate,
+		Read:   resourceAppRead,
+		Update: resourceAppUpdate,
+		Delete: resourceAppDelete,
 
 		Schema: map[string]*schema.Schema{
-			"address": &schema.Schema{
+			"handle": &schema.Schema{
 				Type:     schema.TypeString,
 				Required: true,
 			},
 		},
 	}
+}
+
+// https://www.aptible.com/documentation/enclave/cli/apps-create.html
+func resourceAppCreate(d *schema.ResourceData, m interface{}) error {
+	handle := d.Get("handle").(string)
+	d.SetId(handle)
+	return resourceAppRead(d, m)
+}
+
+func resourceAppRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceAppUpdate(d *schema.ResourceData, m interface{}) error {
+	return resourceAppRead(d, m)
+}
+
+// https://www.aptible.com/documentation/enclave/cli/apps-deprovision.html
+func resourceAppDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
 }
